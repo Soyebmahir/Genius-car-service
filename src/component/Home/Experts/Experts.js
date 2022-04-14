d540e4ac71ebfac6ac642775b50f40c8034a97df
Soyeb <tssoyeb@gmail.com>
2022-04-14 19:51:02 +0600

    added bootstraps css and create card and responsive them

--- src/component/Home/Experts/Experts.js
+++ src/component/Home/Experts/Experts.js
@@ -5,6 +5,7 @@ import expert3 from '../../../images/experts/expert-3.jpg'
 import expert4 from '../../../images/experts/expert-4.jpg'
 import expert5 from '../../../images/experts/expert-5.jpg'
 import expert6 from '../../../images/experts/expert-6.png'
+import Expert from '../Expert/Expert';
 
 const experts =[
     {name:"will smith",id:1,img:expert1},
@@ -18,8 +19,16 @@ const experts =[
 
 const Experts = () => {
     return (
-        <div>
-            <h2>Our Experts</h2>
+        <div className='container '>
+            <h2 className='text-success text-center'>Our Experts</h2>
+            <div className='row'>
+                {
+                    experts.map(expert =><Expert
+                    key={expert.id}
+                    expert={expert}></Expert>)
+                }
+
+            </div>
         </div>
     );
 };
