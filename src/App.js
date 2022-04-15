6c660686d5160c0d7ba536dad98f40f1d4bbd876
Soyeb <tssoyeb@gmail.com>
2022-04-15 15:45:09 +0600

    create login form and route to register

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ import Footer from './component/Footer/Footer';
 import Home from './component/Home/Home/Home';
 import ServiceDetails from './component/ServiceDetails/ServiceDetails';
 import Login from './component/Login/Login';
+import Register from './component/Register/Register';
+
 
 function App() {
-  
+
   return (
     <div >
       <Header></Header>
@@ -19,6 +21,7 @@ function App() {
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/about" element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/service/:serviceId' element={<ServiceDetails></ServiceDetails>}></Route>
       </Routes>
       <Footer></Footer>
