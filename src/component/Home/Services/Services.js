0dc8b51f7a5cc019170c1b2dd5a61f3b566c7e9d
Soyeb <tssoyeb@gmail.com>
2022-04-15 09:12:07 +0600

    some changes

--- src/component/Home/Services/Services.js
+++ src/component/Home/Services/Services.js
@@ -6,7 +6,7 @@ const Services = () => {
     const [services,setServices]=useState([])
 
     useEffect(()=>{
-        fetch('services.json')
+        fetch('Services.json')
         .then(res=>res.json())
         .then(data=>setServices(data))
     },[])
